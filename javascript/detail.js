2cf26847e0f9ee34698452c3e895c4b6e2a562f4
faris <faris@farisdeMacBook-Pro.local>
2019-11-17 19:11:11 -0500

    modify price

---
+++ javascript/detail.js
@@ -0,0 +1,19 @@
+var basic_price=3;
+var size_factor=1;
+var flavor_factor=1;
+
+function change_factor(type, factor){
+	if(type = "size"){
+		size_factor=factor;
+	} else{
+		flavor_factor=factor;
+	}
+	calculate_price();
+}
+
+function calculate_price(){
+	var price = basic_price * size_factor * flavor_factor;
+	price=Math.round(price * 100) / 100;
+	document.getElementById("td_unit_price").innerHTML = "$" + price;
+	document.getElementById("unit_price").value = price;
+}
