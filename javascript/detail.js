606715e1f2072a9089a2d97beb221c04d4ab88b0
faris <faris@farisdeMacBook-Pro.local>
2019-11-28 13:23:26 -0500

    add javascript to verify form data

--- javascript/detail.js
+++ javascript/detail.js
@@ -12,7 +12,20 @@ function change_factor(type, factor){
 
 function calculate_price(){
 	var price = basic_price * size_factor * flavor_factor;
-	price=Math.round(price * 100) / 100;
+	price = Math.round(price * 100) / 100;
 	document.getElementById("td_unit_price").innerHTML = "$" + price;
 	document.getElementById("unit_price").value = price;
+}
+
+
+function check_form(){
+	document.getElementById("error_msg").innerHTML="";
+	quantity=document.getElementById("quantity");
+	if(quantity.value == ""){
+		document.getElementById("error_msg").innerHTML="please input the quantity you want.";
+		return false;
+	}
+
+	document.getElementById("error_msg").innerHTML="done!";
+	return true;
 }
